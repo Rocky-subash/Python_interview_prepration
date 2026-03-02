61c0a869e493c735e55bcf81660dde480c01c893
Subash S <121676898+Rocky-subash@users.noreply.github.com>
2026-03-02 23:31:57 +0530

    Revert "Adding new program files (#2)"
    This reverts commit 96c4a1a9ce796fd94840950ca6e41f41a539a5b9.

--- Remove duplicates.py
+++
@@ -1,4 +0,0 @@
-def duplicates(s):
-    return "".join(dict.fromkeys(s))
-
-print(duplicates("programming"))
--- freqency of charaters.py
+++
@@ -1,7 +0,0 @@
-def frequency(s):
-    fre ={}
-    for char in s:
-        fre[char] = fre.get(char,0)+1
-
-    return fre
-print(frequency("Programminng"))
